4c614153ab58a440966f7086586f352212eb8a1a
Andrea GAY <andrea.gay2020@campus-eni.fr>
2020-09-21 16:53:59 +0200

    TpNinja suite

---
+++ TPNinja/Models/VmSamourai.cs
@@ -0,0 +1,16 @@
+﻿using BOTP6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TPNinja.Models
+{
+    public class VmSamourai
+    {
+        public Samourai samourai { get; set; }
+        public List<Arme> armes { get; set; }
+        public int? armesId { get; set; }
+
+    }
+}
